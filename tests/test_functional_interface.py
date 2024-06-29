743dba052ed172bf9e225ed0071c7501e345ca2c
Agustinus Kristiadi <agustinus@kristia.de>
2024-06-29 13:43:54 -0400

    Add pytest config

---
+++ tests/test_functional_interface.py
@@ -0,0 +1,9 @@
+# model = AutoModelForSeq2SeqLM.from_pretrained("google-t5/t5-base")
+# tokenizer = AutoTokenizer.from_pretrained("google-t5/t5-base")
+
+
+def test_generate():
+    # context = "Translate English to German: My name is Wolfgang and I live in Berlin"
+    # model_inputs = tokenizer(context, return_tensors="pt")
+    # ults.generate(model=model, model_inputs=model_inputs, max_tokens=40)
+    pass
